d4fd3604b426e7b7eea652542607c5402cfbd353
SASLT <GK@THE1GATE.COM>
2024-09-06 16:33:46 +0300

    Uploads

--- JS/main.js
+++ JS/main.js
@@ -150,3 +150,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
